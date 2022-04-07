ac6d7976daad6afad181c272bfe37a4a187be1c7
andrelake <andre.lake@hotmail.com>
2022-04-06 22:20:34 -0300

    remove imports do bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
 
 @NgModule({
@@ -14,7 +13,6 @@ import { NavbarComponent } from './components/shared/navbar/navbar.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
